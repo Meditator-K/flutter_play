f21148d58fdcd3c7f2270afcdd42ce9a73472496
kongyafeng <1272957817@qq.com>
2022-11-22 17:55:24 +0800

    坐标绘制

--- lib/page/paper2_page.dart
+++ lib/page/paper2_page.dart
@@ -38,11 +38,92 @@ class Paper2Paint extends CustomPainter {
     canvas.translate(size.width / 2, size.height / 2);
     _drawGrid(canvas, size);
 
+    _drawAxis(canvas, size);
+
     _drawDot(canvas);
 
     _drawPoints(canvas);
 
     _drawRawPoints(canvas);
+
+    _drawRect(canvas);
+
+    _drawRRect(canvas);
+
+    _drawDRRect(canvas);
+  }
+
+  void _drawDRRect(Canvas canvas) {
+    Paint paint = Paint()..color = Colors.blue;
+    canvas.drawDRRect(
+        RRect.fromRectXY(
+            Rect.fromCenter(center: Offset(110, 130), width: 80, height: 80),
+            10,
+            10),
+        RRect.fromRectXY(
+            Rect.fromCenter(center: Offset(110, 130), width: 60, height: 60),
+            8,
+            8),
+        paint);
+
+    canvas.drawDRRect(
+        RRect.fromRectXY(
+            Rect.fromCenter(center: Offset(110, 130), width: 40, height: 40),
+            8,
+            8),
+        RRect.fromRectXY(
+            Rect.fromCenter(center: Offset(110, 130), width: 20, height: 20),
+            5,
+            5),
+        paint..color = Colors.green);
+  }
+
+  void _drawRRect(Canvas canvas) {
+    Paint paint = Paint()..color = Colors.blue;
+    //中间方块
+    canvas.drawRRect(
+        RRect.fromRectXY(
+            Rect.fromCenter(center: Offset(-0, 130), width: 60, height: 60),
+            10,
+            10),
+        paint);
+    //左上
+    canvas.drawRRect(RRect.fromLTRBXY(-50, 80, -30, 100, 5, 5),
+        paint..color = Colors.deepPurple);
+    //右上
+    canvas.drawRRect(RRect.fromLTRBR(30, 80, 50, 100, Radius.circular(5)),
+        paint..color = Colors.green);
+    //右下
+    canvas.drawRRect(
+        RRect.fromLTRBAndCorners(30, 160, 50, 180,
+            bottomRight: Radius.circular(5)),
+        paint..color = Colors.orange);
+    //左下
+    canvas.drawRRect(
+        RRect.fromRectAndCorners(
+            Rect.fromPoints(Offset(-50, 160), Offset(-30, 180)),
+            bottomLeft: Radius.circular(5)),
+        paint..color = Colors.black);
+  }
+
+  void _drawRect(Canvas canvas) {
+    Paint paint = Paint()..color = Colors.blue;
+    //中间方块
+    canvas.drawRect(
+        Rect.fromCenter(center: Offset(-110, 130), width: 60, height: 60),
+        paint);
+    //左上
+    canvas.drawRect(
+        Rect.fromLTRB(-160, 80, -140, 100), paint..color = Colors.deepPurple);
+    //右上
+    canvas.drawRect(
+        Rect.fromLTWH(-80, 80, 20, 20), paint..color = Colors.green);
+    //右下
+    canvas.drawRect(Rect.fromCircle(center: Offset(-70, 170), radius: 10),
+        paint..color = Colors.orange);
+    //左下
+    canvas.drawRect(Rect.fromPoints(Offset(-160, 160), Offset(-140, 180)),
+        paint..color = Colors.black);
   }
 
   void _drawPoints(Canvas canvas) {
@@ -110,8 +191,33 @@ class Paper2Paint extends CustomPainter {
     canvas.restore();
   }
 
-  void _drawAxis(Canvas canvas,Size size){
-
+  void _drawAxis(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = Colors.blue
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 1;
+    canvas.save();
+    //x轴
+    canvas.drawLine(
+        Offset(-size.width / 2, 0), Offset(size.width / 2, 0), paint);
+    //y轴
+    canvas.drawLine(
+        Offset(0, -size.height / 2), Offset(0, size.height / 2), paint);
+    //x轴箭头
+    canvas.drawLine(
+        Offset(size.width / 2, 0),
+        Offset(size.width / 2 - 10, -7),
+        paint
+          ..strokeWidth = 2
+          ..strokeCap = StrokeCap.round);
+    canvas.drawLine(
+        Offset(size.width / 2, 0), Offset(size.width / 2 - 10, 7), paint);
+    //y轴箭头
+    canvas.drawLine(
+        Offset(0, size.height / 2), Offset(7, size.height / 2 - 10), paint);
+    canvas.drawLine(
+        Offset(0, size.height / 2), Offset(-7, size.height / 2 - 10), paint);
+    canvas.restore();
   }
 
   void _drawGrid(Canvas canvas, Size size) {
